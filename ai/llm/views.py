b8611ca56c7e7735ecd5de3691fc1c52a546cb25
Kaushal Bhat <kaushal.v.bhat@gmail.com>
2024-02-27 16:59:58 +0000

    add boilerplate

--- ai/llm/views.py
+++ ai/llm/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 
 from llm.models import Request
 from llm.serializers import RequestSerializer
@@ -29,7 +30,20 @@ def api_overview(request):
 # @ratelimit(key='ip', rate='500/h')
 def get_recipes(request):
     # asset = RequestSerializer(data=request.data)
-    print(request.data)
+    prompt = request.data['prompt']
+    print(f'prompt: {prompt}')
+
+    res = None
+    sentinel = None
+    if cache.get(prompt, sentinel) is sentinel: # not in cache
+        print("NOT FOUND IN CACHE")
+        response = "hello, world!"
+
+        cache.set(prompt, response, cache_timeout)
+        res = response
+    else:
+        res = cache.get(prompt)
+
 
     # if asset.is_valid():
     #     asset.save(created_by=request.user, update_by=request.user)
@@ -37,4 +51,4 @@ def get_recipes(request):
     # else:
     #     print(asset.errors)
     #     return Response(status=status.HTTP_400_BAD_REQUEST)
-    return Response(request.data, status=status.HTTP_201_CREATED)
+    return Response(res, status=status.HTTP_201_CREATED)
